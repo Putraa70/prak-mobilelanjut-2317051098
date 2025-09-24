f6d465a56c6fdb8d062c172ac8c7212a1121828b
mmputra63 <mmputra63@gmail.com>
2025-09-24 16:29:29 +0000

    hasil tampilan login

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+// ignore: unused_import
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:myapp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('MoLaApp smoke test', (WidgetTester tester) async {
+    // Build aplikasi dan trigger frame pertama
+    await tester.pumpWidget(const MoLaApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Cek kalau ada teks "Welcome Back 👋" dari LoginScreen
+    expect(find.text('Welcome Back 👋'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Cek tombol login muncul
+    expect(find.text('Login'), findsOneWidget);
   });
 }
